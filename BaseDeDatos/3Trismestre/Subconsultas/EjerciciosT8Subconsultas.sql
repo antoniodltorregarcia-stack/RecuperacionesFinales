31b45a2e4d3cc5999bd3fa69024b133f5ffc4da7
Tony <antoniodltorregarcia@gmail.com>
2026-05-28 09:57:12 +0200

    Subconsultas Terminadas

--- BaseDeDatos/3Trismestre/Subconsultas/EjerciciosT8Subconsultas.sql
+++ BaseDeDatos/3Trismestre/Subconsultas/EjerciciosT8Subconsultas.sql
@@ -159,6 +159,7 @@ select * from cliente;
 select * from producto;
 select * from detalle_pedido;
 select * from pago;
+select * from empleado;
 
 /*1. Devuelve el nombre del cliente con mayor límite de crédito*/
 
@@ -195,3 +196,196 @@ from cliente c
 where limite_credito > (select sum(total)
 						from pago p
                         where p.codigo_cliente = c.codigo_cliente);
+                        
+/*5. Devuelve el producto que mas unidades tiene en stock.*/
+
+select *
+from producto
+where cantidad_en_stock = (select max(cantidad_en_stock)
+						   from producto);
+                           
+/*6. Devuelve el producto que menos unidades tiene en stock.*/
+
+select * 
+from producto
+where cantidad_en_stock = (select min(cantidad_en_stock)
+						   from producto);
+
+/*7. Devuelve el nombre, los apellidos y el email de los empleados que están a cargo de 
+Alberto Soria.*/
+
+select nombre, concat_ws(" ", apellido1, apellido2) as apellidos, email
+from empleado
+where codigo_jefe = (select codigo_empleado
+					 from empleado
+                     where concat_ws(" ", nombre, apellido1,apellido2) like "Alberto Soria%");
+                     
+
+-- 8.5.3. Subconsultas con IN y NOT IN
+
+-- Base de datos: tiendadb.
+
+use tiendadb;
+select * from producto;
+
+
+/*1. Devuelve los nombres de los fabricantes que tienen productos asociados. */
+
+select nombre
+from fabricante
+where codigo in (select codigo_fabricante
+				 from producto);
+
+/*2. Devuelve los nombres de los fabricantes que no tienen productos asociados.*/
+
+select nombre
+from fabricante
+where codigo not in (select codigo_fabricante
+					 from producto
+                     where codigo_fabricante is not null);
+                     
+                     
+-- Base de datos: empleadodb.
+
+use empleadodb;
+
+/*1. Devuelve los nombres de los departamentos que tienen empleados asociados.*/
+
+select nombre
+from departamento
+where codigo in (select codigo_departamento
+				 from empleado);
+
+/*2. Devuelve los nombres de los departamentos que no tienen empleados asociados.*/
+
+select nombre
+from departamento
+where codigo not in (select codigo_departamento
+					 from empleado
+                     where codigo_departamento is not null);
+  
+  
+-- Base de datos: ventadb.
+
+use ventasdb;
+
+/*1. Devuelve un listado de los clientes que no han realizado ningún pedido.*/
+
+select *
+from cliente
+where id not in (select id_cliente
+				 from pedido
+                 where id_cliente is not null);
+                 
+/*2. Devuelve un listado de los comerciales que no han realizado ningún pedido.*/
+
+select *
+from comercial
+where id not in (select id_comercial
+				 from pedido
+                 where id_comercial is not null);
+                 
+                 
+-- 8.5.4. Subconsultas con EXISTS y NOT EXISTS.
+
+-- Base de datos: tiendadb.
+
+use tiendadb;
+
+/*1. Devuelve los nombres de los fabricantes que tienen productos asociados. */
+
+select nombre
+from fabricante f
+where exists (select 1
+			  from producto p
+			  where f.codigo = p.codigo_fabricante);
+
+/*2. Devuelve los nombres de los fabricantes que no tienen productos asociados.*/
+
+select nombre
+from fabricante f
+where not exists (select 1 -- El select no se tiene en cuenta
+				  from producto p
+                  where p.codigo_fabricante = f.codigo );
+                  
+                  
+-- Base de datos: empleadodb.
+
+use empleadodb;
+
+/*1. Devuelve los nombres de los departamentos que tienen empleados asociados.*/
+
+select nombre
+from departamento d
+where exists (select 1
+			  from empleado e
+              where e.codigo_departamento = d.codigo);
+              
+              
+/*2. Devuelve los nombres de los departamentos que no tienen empleados asociados.*/
+
+select nombre
+from departamento d
+where not exists (select 1
+			  from empleado e
+              where e.codigo_departamento = d.codigo);
+              
+
+-- Base de datos: ventasdb.
+
+use ventasdb;
+
+/*1. Devuelve un listado de los clientes que no han realizado ningún pedido.*/              
+
+select *
+from cliente c
+where not exists (select 1
+				  from pedido p
+                  where c.id = p.id_cliente);
+
+
+/*2. Devuelve un listado de los comerciales que no han realizado ningún pedido.*/
+
+select *
+from comercial c
+where not exists (select 1
+				  from pedido p
+                  where c.id = p.id_comercial);
+                  
+-- Base de datos: Jardinería.
+
+use jardineria;
+
+/*1. Devuelve un listado que muestre solamente los clientes que no han realizado ningún 
+pago.*/
+
+select *
+from cliente c
+where not exists (select 1
+				  from pago p
+                  where c.codigo_cliente = p.codigo_cliente);
+                  
+/*2. Devuelve un listado que muestre solamente los clientes que sí han realizado ningún 
+pago.*/
+
+select *
+from cliente c
+where exists (select 1
+				  from pago p
+                  where c.codigo_cliente = p.codigo_cliente);
+                  
+/*3. Devuelve un listado de los productos que nunca han aparecido en un pedido.*/
+
+select *
+from producto pr
+where not exists (select 1
+			      from detalle_pedido d
+                  where pr.codigo_producto = d.codigo_producto);
+                  
+/*4. Devuelve un listado de los productos que han aparecido en un pedido alguna vez*/
+
+select *
+from producto pr
+where  exists (select 1
+			   from detalle_pedido d
+			   where pr.codigo_producto = d.codigo_producto);
